e7a0c23127760312075c09fcbec962c15fd8bccb
Frog999 <zx3356@gmail.com>
2022-05-10 23:26:58 +0900

    플레이 기능 추가

--- js/play.js
+++ js/play.js
@@ -1,47 +1,61 @@
-// 3. 이하 함수는 API 코드가 다운로드 된 후에, <iframe>을 생성한다.
-// 높이 360px, 너비 640px로 iframe을 생성하고,
-// videoId M7lc1UVf-VE인 영상이 나타나게 한다.
 let player;
-function exfunc() {
-    for (let i = 0; i < playlist_id.length; i++) {
-        // console.log(playlist_id[i]);
-
-        player = new YT.Player('player_view', {
-            height: '100%',
-            width: '100%',
-            videoId: playlist_id[i],
-            events: {
-                'onReady': onPlayerReady,
-                'onStateChange': onPlayerStateChange
+let playlist_id = [];
+let arrayNum = 0;
+function play() {
+        onYouTubeIframeAPIReady();
+        function onYouTubeIframeAPIReady(){
+            if (playlist_id[arrayNum]){
+                player = new YT.Player('player_view', {
+                    height: '100%',
+                    width: '100%',
+                    videoId: playlist_id[arrayNum],
+                    events: {
+                        'onReady': onPlayerReady,
+                        'onStateChange': onPlayerStateChange
+                    }
+                })
+            }else {
+                alert("곡이 없습니다.")
             }
-        });
-    }
-
-        // 4. IFrame API는 비디오 플레이어가 준비되었을 때, 이하 함수를 호출한다.
-
-        function onPlayerReady(event) {
-            event.target.playVideo();
         }
-
     }
-
-    // 5. IFrame API는 비디오 플레이어의 상태가 변경되었을 때, 이하 함수를 호출한다.
-    // 이하 함수는 영상이 재생 중인지 여부를 가리킨다.
-    // 이하 함수의 값 6000은 이 플레이어가 6초 재생 후 자동으로 멈춰야 함을 의미한다.
+    function onPlayerReady(event) {
+        event.target.playVideo();
+    }
     function onPlayerStateChange(event) {
         if (event.data == YT.PlayerState.ENDED) {
-            setTimeout(playerDestroy, 2000);
+            nextVideo();
         }
     }
     function playerDestroy() {
-        if (isEmpty(player) == false) 
-        {
             player.stopVideo();
             player.destroy();
             player = null;
-        }
     }
+    function nextVideo() {
+        arrayNum++;
+        playerDestroy();
+        play();
+    }
+    function prevVideo(){
+        arrayNum--;
+        playerDestroy();
+        play();
+    }
+    function clearPlaylist(event) {
+        if(player != null){
+            playerDestroy();
+        }
+        playlist_id = [];
+        arrayNum = 0;
+        for (let m = 0; m < order_number; m++) {
+            document.getElementById("main").deleteRow(-1);
+        }
+        order_number = 0;
+    }
+    
+    let b = document.querySelectorAll(".delete_img");
+    b.forEach((item, n) => {item.addEventListener('click', () => deleteList(n))});
+    function deleteList() {
 
-    function stopVideo() {
-        player.stopVideo();
     }
